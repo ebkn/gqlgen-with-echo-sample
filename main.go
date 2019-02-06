b963f1e526ec670627b358d917151a1005f60995
ebkn <ktennis.mqekr12@gmail.com>
2019-02-06 23:09:25 +0900

    feat: create restricted group

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
+
+const authContextKey = "auth"
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	e.POST("/login", login)
+
+	restricted := e.Group("/restricted")
+	restricted.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		Claims:     &jwt.StandardClaims{},
+		ContextKey: authContextKey,
+		SigningKey: []byte("secret"),
+	}))
 	e.Logger.Fatal(e.Start(":3000"))
 }
