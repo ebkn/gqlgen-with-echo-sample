4efe23bdd77b83afe74dbbcd99916bb876e7cbdc
ebkn <ktennis.mqekr12@gmail.com>
2019-02-06 23:59:50 +0900

    feat: add graphql endpoint

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"errors"
+
+	"github.com/99designs/gqlgen/graphql"
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -20,5 +24,26 @@ func main() {
 		ContextKey: authContextKey,
 		SigningKey: []byte("secret"),
 	}))
+	restricted.POST("/graphql", func(c echo.Context) error {
+		h := handler.GraphQL(g.NewExecutableSchema(Config{Resolvers: &Resolver{}}),
+			handler.ResolverMiddleware(func(ctx context.Context, next graphql.Resolver) (interface{}, error) {
+				ctx, err := getCtxWithJWTCtxFromEchoCtx(ctx, c)
+				if err != nil {
+					return nil, err
+				}
+				return next(ctx)
+			}),
+		)
+		h.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
 	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func getCtxWithJWTCtxFromEchoCtx(ctx context.Context, c echo.Context) (context.Context, error) {
+	token, ok := c.Get(authContextKey).(*jwt.Token)
+	if ok == false {
+		return nil, errors.New("auth_context_not_found")
+	}
+	return context.WithValue(ctx, AuthContextKey, token), nil
 }
